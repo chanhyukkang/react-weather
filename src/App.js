cad1baf86d57b278f69b144a56293fb27c7e6fcb
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-18 16:55:59 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import Header from './components/Header.js';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount() {
+    console.log('Component DID MOUNT');
+    }
+
+    render() {
+    const name = "chanhyuk";
+    const foo = "foo";
+
+        return (
+        <div className="App">
+              <Header name = {name}/>
+        </div>
+        );
+    }
+
 }
 
 export default App;
+
+/*ES6 이전버전
+function App() {
+  return (
+
+  );
+}
+*/
+
---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import logo from '../logo.svg';
+
+
+//stateless component, dumb component
+const Header = ({name ,foo}) => {
+
+//const name = props.name;
+
+    return (
+        <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <div>
+             My name is {name}
+           </div>
+           <p>
+             Edit <code>src/App.js</code> and save to reload.
+           </p>
+           <a
+             className="App-link"
+             href="https://reactjs.org"
+             target="_blank"
+             rel="noopener noreferrer"
+           >
+             Learn React
+           </a>
+         </header>
+    );
+}
+
+export default Header;
+/* ES6이전버전
+function Header(){
+   return (
+           <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+       );
+}
+*/
+
